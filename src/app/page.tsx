48f9245ce6aebdf8d24ba43b7e1d1ce7cbd9523d
Santiago Dimaren <sdimaren1@gmail.com>
2024-04-12 09:39:20 -0400

    layout improvements, restructuring nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+'use client'
+import Carousel from "@/components/Carousel";
 import Nav from "@/components/Nav";
+import useWindowUtils from "@/hooks/useWindowUtils";
 
 export default function Home() {
+  const { loaded } = useWindowUtils();
+
   return (
-    <main className="flex flex-col items-center min-h-screen">
-      <Nav />
+    <main className="flex flex-col items-center w-full">
+      {loaded && (
+        <>
+          <Nav />
+          <Carousel />
+        </>
+      )}
     </main>
   );
 }
---
+++ src/components/Carousel.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function Carousel({ }) {
+
+  return (
+    <>
+      <div className='mt-0 w-full h-[549px] bg-[#444444]'></div>
+    </>
+  );
+}
