ff793ae8ab554e69cdc56f935e826cbb0e97a5d7
Gregorio-Moreta <78994816+Gregorio-Moreta@users.noreply.github.com>
2024-05-04 00:03:31 -0400

    integrated stripe API, tested with dummy data, successful requests to src/app/donate-with-checkout, src/app/donate-with-elements, src/app/donate-with-embedded-checkout. All routes load successfully, edited tsconfig to load utils, actions, app, components folder using @ pathing syntax. Added styling to layout.tsx, added loaded to useEffect in hooks/useWindowsUtils.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { draftMode } from "next/headers";
 import VisualEditing from "@/components/VisualEditing";
+import "../../styles.css";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
