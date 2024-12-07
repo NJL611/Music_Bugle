661405e3dc5487b70d463bbe354780a89d3c6eac
Santiago Dimaren <sdimaren1@gmail.com>
2024-12-07 11:50:18 -0500

    Layout updates, alignment with article page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,9 +6,9 @@ import LogoFooter from 'public/svgs/LogoFooter';
 export default function Footer() {
     return (
         <>
-            <footer className="bg-[#1B1B1B] w-full px-12 pt-9 pb-6 text-white flex justify-between flex-col text-sm">
-                <div className="w-full mx-auto flex flex-col md:flex-row items-start gap-6">
-
+            <footer className="bg-[#1B1B1B] w-full lg:px-24 mx-auto pt-8 px-6 pb-6 text-white text-sm">
+                <div className="w-full mx-auto grid grid-cols-1 md:grid-cols-3 gap-6">
+                    {/* First Column */}
                     <div className="flex flex-col items-start min-w-[270px] max-w-[355px]">
                         <LogoFooter color="white" />
                         <span className="mb-4 mt-4 text-sm">
@@ -17,7 +17,8 @@ export default function Footer() {
                         <Button text="Donate" href="#" bgColor="#ffffff" textColor="black" />
                     </div>
 
-                    <div className="flex flex-col md:flex-row justify-center items-start md:items-center flex-1 gap-8 md:gap-14 w-full my-5">
+                    {/* Second Column */}
+                    <div className="grid grid-cols-2 gap-8 md:gap-14 w-full my-5 ">
                         <div>
                             <ul>
                                 <li><Link href="#" className="hover:underline">News</Link></li>
@@ -36,18 +37,20 @@ export default function Footer() {
                         </div>
                     </div>
 
-                    <div className="flex flex-col items-start md:items-end">
+                    {/* Third Column */}
+                    <div className="flex flex-col items-start justify-between md:items-end">
                         <span className="font-bold md:text-right text-sm">Subscribe to Our Newsletter</span>
-                        <span className="mt-2 mb-4 md:text-right text-sm">Sign up for email updates on the latest music, talks, and events.</span>
-                        <div className="w-full flex justify-end">
-                            <input
-                                type="email"
-                                placeholder="Enter your email here"
-                                className="w-full px-4 py-2 border border-gray-400 text-black text-[14px] rounded-md focus:outline-none"
-                            />
+                        <div className="">
+                            <span className="mt-2 inline-block md:text-right text-sm justify-end">Sign up for email updates on the latest music, talks, interviews, live events and shows.</span>
+                            <div className="w-full flex justify-end mt-3">
+                                <input
+                                    type="email"
+                                    placeholder="Enter your email here"
+                                    className="w-full px-4 py-2 border border-gray-400 text-black text-[14px] rounded-md focus:outline-none"
+                                />
+                            </div>
                         </div>
                     </div>
-
                 </div>
 
                 <div className="border-t border-gray-700 mt-6 pt-4 flex flex-col md:flex-row justify-between items-center">
