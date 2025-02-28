1e824fab2fdcda9a9189596243f87ee5d68d5f6a
Gregorio-Moreta <gregoriomoreta4@gmail.com>
2025-02-28 15:44:31 -0500

    created the donate page, added styling, state

--- dist/static/BroadcastDisplayedDocument-241471e9.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./sanity-81459e8b.js";import{m as e}from"./DisplayedDocumentBroadcaster-f4a22b37.js";var l=r.memo(function(o){const t=e();return r.useEffect(()=>t==null?void 0:t(o.value),[o.value,t]),null});export{l as default};
