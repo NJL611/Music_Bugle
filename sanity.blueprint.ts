e34dba75d70d987ad24753830ca29a18a4c63590
Santiago Dimaren <santiago.dimaren@gmail.com>
2025-06-13 23:33:39 -0400

    Update Sanity, implement Sanity functions and blueprints in order to auto generate subtitles and tags if none exist

---
+++ sanity.blueprint.ts
@@ -0,0 +1,16 @@
+import { defineBlueprint, defineDocumentFunction } from '@sanity/blueprints'
+
+export default defineBlueprint({
+  resources: [
+    defineDocumentFunction({
+      name: 'enrich-post',
+      timeout: 30,
+      event: {
+        on: ['publish'],
+        filter: `_type == "post"`,
+        projection: 'title, _id, _type, body'
+      }
+    })
+
+  ]
+})
